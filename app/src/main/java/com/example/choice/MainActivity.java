fd40d1a2473d4b89caf2ec67adc001ee497a8fda
Ziwen <xiezw@bu.edu>
2021-01-06 20:44:35 -0500

    add navigation fragments

--- app/src/main/java/com/example/choice/MainActivity.java
+++ app/src/main/java/com/example/choice/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.choice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button name = (Button) findViewById(R.id.name); // initilization: get the start button
+
+        name.setOnClickListener(new View.OnClickListener() { // what happens when click
+            @Override
+            public void onClick(View v) {
+                Intent next = new Intent(MainActivity.this,randname.class); //set intent for going to playerinfo class
+                startActivity(next); // click to continue to the next activity
+                //player.pause();
+            }
+        });
     }
 }
